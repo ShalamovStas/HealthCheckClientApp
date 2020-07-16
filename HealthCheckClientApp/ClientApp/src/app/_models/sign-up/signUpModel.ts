d6bf8e12d7cca5ce074da2415b0d58c4accdfb42
Stanislav Shalamov <stanislav.shalamov@ssa.group>
2020-07-16 14:12:45 +0300

    added signUpModel

---
+++ HealthCheckClientApp/ClientApp/src/app/_models/sign-up/signUpModel.ts
@@ -0,0 +1,11 @@
+export interface SignUpModel{
+    email: string;
+    password: string;
+    phone: string;
+    firstName: string;
+    lastName: string;
+    sex: string;
+    dateOfBirth: string;
+    country: string;
+    facilityCode: string;
+}
